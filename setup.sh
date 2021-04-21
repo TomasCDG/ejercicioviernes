a9ecc6406cb9e3c70e155ed191ca02fbec92918b
Tomás Di Gennaro <tomas.digennaro@gmail.com>
2021-04-21 13:41:37 -0300

    correcting

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"tomas.digennaro@gmail.com\"\n\
+email = \"${HEROKU_EMAIL_ADDRESS}\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
